7d033beaf8069c9a931e1bb76aa326796bec9bc0
Jefferson Abreu Martinez <abreumartinezjefferson@gmail.com>
2023-10-16 21:19:19 -0400

    Add project files.

---
+++ API/Controllers/Books.cs
@@ -0,0 +1,39 @@
+﻿using API.Controllers.General;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    public class Books : GeneralApiControllerBase
+    {
+        [HttpGet()]
+        public async Task<IActionResult> GetAllBooks()
+        {
+            return Ok();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetBookById()
+        {
+            return Ok();
+        }
+
+        [HttpPost()]
+        public async Task<IActionResult> CreateBook()
+        {
+            return Ok();
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateBook()
+        {
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteBook()
+        {
+            return Ok();
+        }
+
+    }
+}
