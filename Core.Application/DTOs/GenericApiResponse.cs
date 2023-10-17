d0b3fabba27f0eb81d64d4c18f2126779585d02a
Jefferson Abreu Martinez <abreumartinezjefferson@gmail.com>
2023-10-17 09:36:27 -0400

    GenericResponses

---
+++ Core.Application/DTOs/GenericApiResponse.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Application.DTOs
+{
+    public class GenericApiResponse<DTO>
+    {
+        public HttpStatusCode StatusCode { get; set; }
+        public DTO Data { get; set; }
+        public string Message { get; set; }
+    }
+}
--- Core.Application/Services/API/APIService.cs
+++ Core.Application/Services/API/APIService.cs
@@ -1,6 +1,8 @@
 ﻿using Core.Application.DTOs;
 using Newtonsoft.Json;
 using System;
+using System.Collections.Generic;
+using System.Net;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,74 +18,56 @@ namespace Core.Application.Services.API
             _httpClientFactory = httpClientFactory;
         }
 
-        public async Task<List<InfoBookDTO>> GetBooks()
+        public async Task<GenericApiResponse<List<InfoBookDTO>>> GetBooks()
         {
             var client = _httpClientFactory.CreateClient("BackEnd");
-            var response = await client.GetAsync("/api/v1/Books");
+            var call = await client.GetAsync("/api/v1/Bookss");
+            var response = await HttpMessages<List<InfoBookDTO>>(call);
 
-            if (response.IsSuccessStatusCode)
-            {
-                var jsonContent = await response.Content.ReadAsStringAsync();
-                List<InfoBookDTO> booksList = JsonConvert.DeserializeObject<List<InfoBookDTO>>(jsonContent);
-                return booksList;
-            }
-            else
-            {
-                throw new Exception($"Error al obtener la lista de libros. Código de estado: {response.StatusCode}");
-            }
+            var jsonContent = await call.Content.ReadAsStringAsync();
+            List<InfoBookDTO> booksList = JsonConvert.DeserializeObject<List<InfoBookDTO>>(jsonContent);
+            response.Data = booksList;
+            return response;
         }
 
-
-        public async Task<InfoBookDTO> GetBookById(int Id)
+        public async Task<GenericApiResponse<InfoBookDTO>> GetBookById(int Id)
         {
             var client = _httpClientFactory.CreateClient("BackEnd");
-            var response = await client.GetAsync($"/api/v1/Books/{Id}");
-            if (response.IsSuccessStatusCode)
-            {
-                var jsonContent = await response.Content.ReadAsStringAsync();
-                InfoBookDTO book = JsonConvert.DeserializeObject<InfoBookDTO>(jsonContent);
-                return book;
-            }
-            else
-            {
-                throw new Exception($"Error al obtener la lista de libros. Código de estado: {response.StatusCode}");
-            }
+            var call = await client.GetAsync($"/api/v1/Books/{Id}");
+            var response = await HttpMessages<InfoBookDTO>(call);
+
+            var jsonContent = await call.Content.ReadAsStringAsync();
+            InfoBookDTO book = JsonConvert.DeserializeObject<InfoBookDTO>(jsonContent);
+            response.Data = book;
+            return response;
         }
 
-        public async Task<InfoBookDTO> AddBook(BookDTO request)
+        public async Task<GenericApiResponse<InfoBookDTO>> AddBook(BookDTO request)
         {
             var client = _httpClientFactory.CreateClient("BackEnd");
             var bookJson = JsonConvert.SerializeObject(request);
             var content = new StringContent(bookJson, Encoding.UTF8, "application/json");
-            var response = await client.PostAsync("/api/v1/Books", content);
-            if (response.IsSuccessStatusCode)
-            {
-                var jsonContent = await response.Content.ReadAsStringAsync();
-                InfoBookDTO book = JsonConvert.DeserializeObject<InfoBookDTO>(jsonContent);
-                return book;
-            }
-            else
-            {
-                throw new Exception($"Error al obtener la lista de libros. Código de estado: {response.StatusCode}");
-            }
+            var call = await client.PostAsync("/api/v1/Books", content);
+            var response = await HttpMessages<InfoBookDTO>(call);
+
+            var jsonContent = await call.Content.ReadAsStringAsync();
+            InfoBookDTO book = JsonConvert.DeserializeObject<InfoBookDTO>(jsonContent);
+            response.Data = book;
+            return response;
         }
 
-        public async Task<InfoBookDTO> UpdateBook(int Id, BookDTO request)
+        public async Task<GenericApiResponse<InfoBookDTO>> UpdateBook(int Id, BookDTO request)
         {
             var client = _httpClientFactory.CreateClient("BackEnd");
             var bookJson = JsonConvert.SerializeObject(request);
             var content = new StringContent(bookJson, Encoding.UTF8, "application/json");
-            var response = await client.PutAsync($"/api/v1/Books/{Id}", content);
-            if (response.IsSuccessStatusCode)
-            {
-                var jsonContent = await response.Content.ReadAsStringAsync();
-                InfoBookDTO book = JsonConvert.DeserializeObject<InfoBookDTO>(jsonContent);
-                return book;
-            }
-            else
-            {
-                throw new Exception($"Error al obtener la lista de libros. Código de estado: {response.StatusCode}");
-            }
+            var call = await client.PutAsync($"/api/v1/Books/{Id}", content);
+            var response = await HttpMessages<InfoBookDTO>(call);
+
+            var jsonContent = await call.Content.ReadAsStringAsync();
+            InfoBookDTO book = JsonConvert.DeserializeObject<InfoBookDTO>(jsonContent);
+            response.Data = book;
+            return response;
         }
 
         public async Task<System.Net.HttpStatusCode> DeleteBook(int Id)
@@ -91,6 +75,16 @@ namespace Core.Application.Services.API
             var client = _httpClientFactory.CreateClient("BackEnd");
             var response = await client.DeleteAsync($"/api/v1/Books/{Id}");
             return response.StatusCode;
+        }
+
+
+        private async Task<GenericApiResponse<T>> HttpMessages<T>(HttpResponseMessage call) where T : new()
+        {
+            var response = new GenericApiResponse<T>();
+            response.StatusCode = call.StatusCode;
+            response.Message = call.StatusCode.ToString();
+            response.Data = new T();
+            return response;
         }
 
     }
